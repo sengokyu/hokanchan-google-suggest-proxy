9edf5aafcf95ed7d7684f642ff0042b0edf45356
sengokyu <sengokyu+gh@gmail.com>
2019-08-24 23:34:09 +0900

    update webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -3,6 +3,8 @@ const AwsSamPlugin = require('aws-sam-webpack-plugin');
 const awsSamPlugin = new AwsSamPlugin();
 
 module.exports = {
+  mode: process.env.NODE_ENV || 'development',
+  devtool: 'source-map',
   entry: awsSamPlugin.entry(),
   resolve: {
     extensions: ['.ts', '.tsx', '.js']
