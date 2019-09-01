b41b79e4845c8c070789cbf81343d798ed5338cb
sengokyu <sengokyu+gh@gmail.com>
2019-09-01 10:56:58 +0900

    Make return value to be sorted

--- src/google-suggest-proxy/suggestion.service.spec.ts
+++ src/google-suggest-proxy/suggestion.service.spec.ts
@@ -1,13 +1,17 @@
 import { AxiosResponse } from 'axios';
 import { of } from 'rxjs';
 import { SuggestionService } from './suggestion.service';
+import { delay } from 'rxjs/operators';
+import { SuggestApiWrapper } from './suggest-api-wrapper';
 
 const letters = 'あいうえおかきくけこさしすせそたちつてとなにぬねのはひふへほまみむめもやゆよらりるれろわを'.split(
   ''
 );
 
+const getRandomInt = (): number => Math.floor(Math.random() * 500);
+
 describe('SuggestionService', () => {
-  let suggestApi;
+  let suggestApi: jasmine.SpyObj<SuggestApiWrapper>;
   let instance: SuggestionService;
 
   beforeEach(() => {
@@ -23,7 +27,9 @@ describe('SuggestionService', () => {
     <CompleteSuggestion><suggestion data="hoge fuga"/></CompleteSuggestion>
     </toplevel>`;
 
-    suggestApi.getSuggestion.and.returnValue(of(data));
+    // HTTPリクエストごとにランダムな待ちを入れて、順番を保てないようにする
+    const returnValues = letters.map(_ => of(data).pipe(delay(getRandomInt())));
+    suggestApi.getSuggestion.and.returnValues(...returnValues);
 
     const expected = letters.map(i => {
       return { letter: i, result: ['hoge fuga'] };
--- src/google-suggest-proxy/suggestion.service.ts
+++ src/google-suggest-proxy/suggestion.service.ts
@@ -1,4 +1,4 @@
-import { from, Observable } from 'rxjs';
+import { from, Observable, generate } from 'rxjs';
 import { map, mergeMap, toArray } from 'rxjs/operators';
 import { parseSuggestionXml } from './parse-suggestion-xml';
 import { SuggestApiWrapper } from './suggest-api-wrapper';
@@ -7,8 +7,10 @@ import { Suggestion } from './suggestion.model';
 const letters = 'あいうえおかきくけこさしすせそたちつてとなにぬねのはひふへほまみむめもやゆよらりるれろわを'.split(
   ''
 );
+const lettersLength = letters.length;
 
 interface TmpResult {
+  index: number;
   letter: string;
   data: any;
 }
@@ -17,26 +19,37 @@ export class SuggestionService {
   constructor(private suggestApi: SuggestApiWrapper) {}
 
   getSuggestions(word: string): Observable<Suggestion[]> {
-    return from(letters).pipe(
-      mergeMap(letter => this.callApi(word, letter)),
+    return generate(0, x => x < lettersLength, x => ++x).pipe(
+      mergeMap(index => this.callApi(word, index)),
       mergeMap(tmp => this.parseXml(tmp)),
-      toArray()
+      toArray(),
+      map(tmpArray => this.sortAndConvert(tmpArray))
     );
   }
 
-  private callApi(word: string, letter: string): Observable<TmpResult> {
+  private callApi(word: string, index: number): Observable<TmpResult> {
+    const letter = letters[index];
+
     return this.suggestApi.getSuggestion(word + ' ' + letter).pipe(
       map(data => {
-        return { letter, data };
+        return { index, letter, data };
       })
     );
   }
 
-  private parseXml(tmp: TmpResult): Observable<Suggestion> {
+  private parseXml(tmp: TmpResult): Observable<TmpResult> {
     return parseSuggestionXml(tmp.data).pipe(
       map(parsed => {
-        return { letter: tmp.letter, result: parsed };
+        return { index: tmp.index, letter: tmp.letter, data: parsed };
       })
     );
+  }
+
+  private sortAndConvert(tmpArray: TmpResult[]): Suggestion[] {
+    tmpArray.sort((a, b) => a.index - b.index);
+
+    return tmpArray.map(i => {
+      return { letter: i.letter, result: i.data };
+    });
   }
 }
