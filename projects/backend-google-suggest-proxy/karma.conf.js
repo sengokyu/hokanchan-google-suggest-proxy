929530392f7050f4fb55835e2f78587a440b919a
sengokyu <sengokyu+gh@gmail.com>
2019-09-16 19:50:55 +0900

    Re construct directory

--- projects/backend-google-suggest-proxy/karma.conf.js
+++ projects/backend-google-suggest-proxy/karma.conf.js
@@ -14,7 +14,7 @@ module.exports = function(config) {
     files: ['src/**/*.ts'],
 
     // list of files / patterns to exclude
-    exclude: ['src/*/app.ts'],
+    exclude: ['src/**/app.ts'],
 
     // preprocess matching files before serving them to the browser
     // available preprocessors: https://npmjs.org/browse/keyword/karma-preprocessor
